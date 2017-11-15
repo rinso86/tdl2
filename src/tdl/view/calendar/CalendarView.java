ee450cd7dd204fcbc65b47f24f7138a43e3f4c0c
Michael Langbein <michael.langbein@lfu.bayern.de>
2017-11-15 17:16:54 +0100

    Scheduler being displayed

--- src/tdl/view/calendar/CalendarView.java
+++ src/tdl/view/calendar/CalendarView.java
@@ -1,44 +1,50 @@
 package tdl.view.calendar;
 
-import java.awt.Component;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
+
 
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 
 import tdl.controller.Controller;
 import tdl.messages.Message;
 import tdl.messages.Recipient;
-import tdl.model.Task;
 import tdl.utils.scheduler.ScheduleItem;
 
 public class CalendarView implements Recipient {
 	
 	private Controller controller;
-	private JEditorPane jep;
+	private JTextPane jep;
 	private JPanel jp;
 
 	public CalendarView(Controller controller) {
 		this.controller = controller; 
-		this.jp = new JPanel();
+		this.jp = new JPanel(new GridBagLayout());
 		
-		jep = new JEditorPane();
+		jep = new JTextPane();
 		jep.setEditable(false);
-		jp.add(jep);
+		
+		GridBagConstraints bigFieldConstraints = new GridBagConstraints();
+        bigFieldConstraints.gridx = 0;
+        bigFieldConstraints.gridy = 0;
+        bigFieldConstraints.gridwidth = 1;
+        bigFieldConstraints.weightx = bigFieldConstraints.weighty = 1.0;
+        bigFieldConstraints.fill = GridBagConstraints.BOTH;
+        
+		jp.add(jep, bigFieldConstraints);
 		
 		updateDisplay();
 	}
 
 	@Override
 	public void receiveMessage(Message message) {
-		System.out.println("UCalendarView now receiving " + message.getMessageType());
+		System.out.println("CalendarView now receiving " + message.getMessageType());
 		switch(message.getMessageType()) {
 		case UPDATED_TASK:
-			updateDisplay();
-			break;
 		case ADDED_SUBTASK:
 			updateDisplay();
 			break;
@@ -67,8 +73,9 @@ public class CalendarView implements Recipient {
 		for(ScheduleItem si : schedule) {
 			formatted += si.getFrom() + " - ";
 			formatted += si.getTo() + " : ";
-			formatted += si.getTask().getTitle() + " <br/>";
+			formatted += si.getTask().getTitle() + " \n";
 		}
+		formatted += "";
 		return formatted;
 	}
 }
