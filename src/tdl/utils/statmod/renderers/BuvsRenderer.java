1f5d7371b3f9f650b15209198a9f4765b5ba1f91
Michael Langbein <michael.langbein@lfu.bayern.de>
2017-11-27 16:04:47 +0100

    first steps with jfreechart

--- src/tdl/utils/statmod/renderers/BuvsRenderer.java
+++ src/tdl/utils/statmod/renderers/BuvsRenderer.java
@@ -2,6 +2,13 @@ package tdl.utils.statmod.renderers;
 
 import javax.swing.JPanel;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+
 import tdl.utils.statmod.Buvs;
 
 public class BuvsRenderer implements ModRenderer {
@@ -14,6 +21,11 @@ public class BuvsRenderer implements ModRenderer {
 
 	@Override
 	public JPanel render() {
+		CategoryDataset dataset = null;
+		JFreeChart barChart = ChartFactory.createBarChart("Histogram", "X Axis", "Y Axis", dataset, PlotOrientation.HORIZONTAL, true, true, false);
+		ChartFrame cf = new ChartFrame("The histogram", barChart);
+		ChartPanel cp = cf.getChartPanel();
+		
 		return new JPanel();
 	}
 
