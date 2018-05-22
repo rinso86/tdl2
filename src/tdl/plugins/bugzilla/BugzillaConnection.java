078974297d0674d3b0535defe0169ac42745709f
Michael Langbein <michael.langbein@lfu.bayern.de>
2018-05-22 15:44:10 +0200

    Adding unittests for bugzilla integration

--- src/tdl/plugins/bugzilla/BugzillaConnection.java
+++ src/tdl/plugins/bugzilla/BugzillaConnection.java
@@ -1,19 +1,51 @@
 package tdl.plugins.bugzilla;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
 import tdl.model.MutableTask;
 import tdl.model.Task;
 
+
+
+/**
+ * 
+ * https://wiki.mozilla.org/Bugzilla:REST_API
+ *
+ */
+
 public class BugzillaConnection {
 
+	private URL bugzillaUrl;
+	private String userEmail;
+	private String userPassword;
+	private CloseableHttpClient httpclient;
+	
 	/**
 	 * Versichere, dass wir auf die REST API von bugzilla zugreifen können
 	 * 
 	 * @param bugzillaUrl
 	 * @param userEmail
 	 * @param userPassword
+	 * 
+	 * @throws MalformedURLException 
 	 */
-	public BugzillaConnection(String bugzillaUrl, String userEmail, String userPassword) {
-		// TODO Auto-generated constructor stub
+	public BugzillaConnection(String bugzillaUrl, String userEmail, String userPassword) throws MalformedURLException {
+		this.bugzillaUrl = new URL(bugzillaUrl);
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+		this.httpclient = HttpClients.createDefault();
 	}
 
 	/**
@@ -35,6 +67,36 @@ public class BugzillaConnection {
 	public Task[] getNewTasks(Task baseTask) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private JSONObject executeQuery(HashMap<String, String> paras) {
+		// @TODO: 
+		
+		// Create uri from paras
+		// get String by execureGet
+		// parse String into JSON object
+		return null;
+	}
+	
+	private String executeGet(URI request) throws Exception {
+		String body = "";
+		HttpGet httpget = new HttpGet(request);
+		CloseableHttpResponse response = httpclient.execute(httpget);
+		try {
+			HttpEntity entity = response.getEntity();
+		    if (entity != null) {
+		        long len = entity.getContentLength();
+		        if (len != -1) {
+		        	body = EntityUtils.toString(entity);
+		        } else {
+		        	throw new Exception("No response!");
+		        }
+		    }
+		} catch (Exception ex) {
+		    response.close();
+		    throw ex;
+		}
+		return body;
 	}
 
 }
---
+++ test/tdl/plugins/bugzilla/BugzillaConnectionTest.java
@@ -0,0 +1,19 @@
+package tdl.plugins.bugzilla;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BugzillaConnectionTest {
+	
+	@Test
+	public void executeGetTest() {
+		// TODO
+	}
+
+	@Test
+	public void executeQueryTest() {
+		// TODO
+	}
+	
+}
