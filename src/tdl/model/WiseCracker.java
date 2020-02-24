2224493ad7ad1bfd62b7ebe5d4f56074c7cccb72
Langbein <Michael.Langbein@dlr.de>
2020-02-24 13:15:29 +0100

    fixed typo

--- src/tdl/model/WiseCracker.java
+++ src/tdl/model/WiseCracker.java
@@ -10,7 +10,7 @@ public class WiseCracker {
 		wiseStuff = new ArrayList<String>();
 		wiseStuff.add("We're all goofy.");
 		wiseStuff.add("If you're not feeling stupid, you're not learning.");
-		wiseStuff.add("Perfection is reached not when there is nothing more to add, but when there is nothing more to substract.");
+		wiseStuff.add("Perfection is reached not when there is nothing more to add, but when there is nothing more to subtract.");
 		wiseStuff.add("You can easily stay focused on your task, not by power of will, but by understanding that you don't need to do any of those distracting things, and that they won't make you happy.");
 		wiseStuff.add("You sometimes get distracted by things that are more interesting. If that happens, just put on a youtube-video to listen to in the background. It will draw your subconscious away from what distracted you.");
 		wiseStuff.add("Resistance against a task takes up tons of energy. Instead of resisting, focus on the aspects of a task that you like.");
